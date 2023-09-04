843402dd903023df041ecc2b496b6b0e28d17e86
TWECHY <twechy89@gmail.com>
2023-09-04 21:33:45 +0200

    added managers domain tests

--- Tests.DoaminTests/ProjectServiceTests.cs
+++ Tests.DoaminTests/ProjectServiceTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MITT.API.Controllers;
 using MITT.EmployeeDb;
@@ -6,14 +7,14 @@ using MITT.Services;
 using MITT.Services.Abstracts;
 using Moq;
 
-namespace Tests.DoaminTests;
+namespace TestUnit;
 
 [TestFixture]
 public class ProjectServiceTests
 {
     private DbContextOptions<ManagementDb> _options;
     private ManagementDb _dbContext;
-    private ProjectsController _projectsController;
+    private ProjectsController _controller;
 
     private readonly List<Project> SeedData = new()
     {
@@ -26,7 +27,7 @@ public class ProjectServiceTests
     {
         // Set up an in-memory database for testing
         _options = new DbContextOptionsBuilder<ManagementDb>()
-            .UseInMemoryDatabase(databaseName: "ManagementTestDb")
+            .UseInMemoryDatabase(databaseName: $"{nameof(ManagementDb)}-Test")
             .Options;
         _dbContext = new ManagementDb(_options);
 
@@ -34,22 +35,14 @@ public class ProjectServiceTests
         _dbContext.Projects.AddRange(SeedData);
         _dbContext.SaveChanges();
 
-        _projectsController = new ProjectsController(new ProjectsService(_dbContext), new ManagerService(_dbContext));
-    }
-
-    [TearDown]
-    public void TearDown()
-    {
-        // Clean up the in-memory database after each test
-        _dbContext.Database.EnsureDeleted();
-        _dbContext.Dispose();
+        _controller = new ProjectsController(new ProjectsService(_dbContext), new ManagerService(_dbContext));
     }
 
     [Test]
     public void GetProjectsReturnValueOrEmptyList()
     {
         // Act
-        var result = _projectsController.Projects().Result;
+        var result = _controller.Projects().Result;
 
         // Assert
         Assert.That(result, Is.Not.Null);
@@ -75,7 +68,7 @@ public class ProjectServiceTests
         };
 
         // Act
-        _projectsController.AddOrUpdateProject(newProject).Wait();
+        _controller.AddOrUpdateProject(newProject).Wait();
         var result = _dbContext.Projects.FirstOrDefault(e => e.Bank == newProject.Bank);
 
         // Assert
@@ -100,10 +93,57 @@ public class ProjectServiceTests
         };
 
         // Act
-        var dataResult = _projectsController.AddOrUpdateProject(newProject).Result;
+        var dataResult = _controller.AddOrUpdateProject(newProject).Result;
 
         // Assert
         Assert.That(dataResult, Is.Not.Null);
+        Assert.That(dataResult, Is.InstanceOf<OperationResult>());
         Assert.That(dataResult.Type, Is.EqualTo(OperationResult.ResultType.Failure));
+    }
+
+    [Test]
+    public void GetManagersReturnValue()
+    {
+        // Act
+        var result = _controller.Managers().Result;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.InstanceOf<List<ProjectManagerVm>>());
+        Assert.That(result, Has.Count.AtMost(1));
+    }
+
+    [Test]
+    public void Add_ValidAddOrUpdateManager()
+    {
+        // Arrange
+        var newManager = new ProjectManagerDto
+        {
+            FullName = "abdalsalam aweid",
+            NickName = "absy",
+            Email = "a.aweid@masarat.ly",
+            Phone = "0924228018"
+        };
+
+        // Act
+        var valueResult = _controller.AddOrUpdateManager(newManager).Result;
+        var result = _dbContext.Managers.FirstOrDefault(e => e.NickName == "absy");
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Not.Null);
+            Assert.That(valueResult, Is.TypeOf<OperationResult>());
+            Assert.That(result.NickName, Is.EqualTo(newManager.NickName));
+            Assert.That(result.Phone, Is.EqualTo(newManager.Phone));
+        });
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Clean up the in-memory database after each test
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Dispose();
     }
 }
